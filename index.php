fbcff7e81de1c3910ca827475fc5134a841c1c3a
leo <leocv0205@gmail.com>
2020-06-28 23:04:34 -0500

    creacion de css

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos.css">
     <title >FORMULARIO</title>
 </head>
 <body>
@@ -42,7 +43,7 @@
                     <br>
                         <div class="row">
                             <div class="col">
-                                <button type="submit" class="btn btn-primary">AGREGAR</button>
+                                <button id="agregar" type="submit" class="btn btn-primary">AGREGAR</button>
                             </div>
                         </div>
                 </form>
